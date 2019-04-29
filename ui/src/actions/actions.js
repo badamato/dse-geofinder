0656522ad1ad5618fafeecb2310123e4a4c73f77
Beth Damato <bethadamato@gmail.com>
2019-04-29 10:53:47 -0400

    defining action to get database data

--- ui/src/actions/actions.js
+++ ui/src/actions/actions.js
@@ -1,8 +1,25 @@
 import {createAction} from 'redux-actions';
-
 import requestActions from './requestactions';
 import {get} from '../common/requests';
 import {changeScreen} from './navigationactions';
+
+
+export function getGeoData() {
+    return(dispatch, getState) => {
+        const url = '';
+        const interval = setInterval(() => {
+            get({
+                url: url, 
+                success: function(res){
+
+                },
+                dispatch: dispatch,
+            });
+        }, 5000)
+    }
+}
+
+
 
 export function updateValue(key, value){
     return(dispatch, getState) => {
