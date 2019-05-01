7c95dff12c0e1f1f2df54a57c654edd542c1db86
Beth Damato <bethadamato@gmail.com>
2019-05-01 00:00:21 -0400

    adding map pins - dummy coords

--- ui/src/components/Map/reactmap.js
+++ ui/src/components/Map/reactmap.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import Pin from './pin';
+import secrets from '../../secrets/secrets';
 
 class ReactMap extends Component {
     constructor(props) {
@@ -9,22 +11,39 @@ class ReactMap extends Component {
             viewport: {
                 width: '100%',
                 height: 815,
-                latitude: 33.753746,
-                longitude: -84.386330,
-                zoom: 9
+                latitude: 33.758447,
+                longitude: -84.386171,
+                zoom: 13
             },
-            token: 'pk.eyJ1IjoiYmFkYW1hdG8iLCJhIjoiY2p1anZ6YTVkMXBzZTQ0dWpheGF4ODF6dyJ9.KglfXQnMkcHnkKPyr-ZkXw'
+            token: MapboxAccessToken,
+            coords: [
+                { latitude: 33.760702, longitude: -84.387473 },
+                { latitude: 33.755368, longitude: -84.389651 },
+                { latitude: 33.758106, longitude: -84.394737 },
+                { latitude: 33.761638, longitude: -84.387924 },
+                { latitude: 33.761367, longitude: -84.387826 },
+                { latitude: 33.763982, longitude: -84.392621 }
+            ]
         };
     }
 
 
     render() {
+        const { coords } = this.state;
+
         return (
         <ReactMapGL
             mapboxApiAccessToken={this.state.token}
             {...this.state.viewport}
-            onViewportChange={(viewport) => this.setState({viewport})}
-        />
+            onViewportChange={(viewport) => this.setState({viewport})}>
+
+            { coords.map((coord, index) => (
+                <Marker key={index} latitude={coord.latitude} longitude={coord.longitude} >
+                    <Pin />
+                </Marker>
+            )) }
+
+        </ReactMapGL>
         );
     }
 }
