6768545d03a250c7701411361534c50c2545071c
Beth Damato <bethadamato@gmail.com>
2019-05-01 22:43:02 -0400

    adds map styling

--- ui/src/components/Map/controlpanel.js
+++ ui/src/components/Map/controlpanel.js
@@ -1,35 +1,32 @@
-
 import React, {PureComponent} from 'react';
 import style from '../../style/style.css';
 
 const defaultContainer = ({children}) => <div className="control-panel">{children}</div>;
 
 
-export default class ControlPanel extends PureComponent {
-    // renderEvent = (eventName) => {
-    //     const {events = {}} = this.props;
-    //     const lngLat = events[eventName];
-
-
-    //     return (
-    //     <div key={eventName}>
-    //         <strong>{eventName}:</strong>{' '}
-    //         {lngLat ? lngLat.map(round5).join(', ') : <em>null</em>}
-    //     </div>
-    //     );
-    // };
+class ControlPanel extends PureComponent {
 
     render() {
         const Container = this.props.containerComponent || defaultContainer;
+        const {allDay, onChangeAllDay} = this.props;
+
         return (
-        <Container>
-            <h3>DEMOGRAPHIC PANEL</h3>
-            {/* <p>Click to add income layer.</p> */}
-            <hr />
-            <div>
-            {/* {eventNames.map(this.renderEvent)} */}
-            </div>
-        </Container>
+            <Container>
+                <h3>DEMOGRAPHICS PANEL</h3>
+                {/* <p>Click to add income layer.</p> */}
+                <hr />
+                <div className="input">
+                    <label>Low Income Areas </label>
+                    <input type="checkbox"
+                        name="geodata"
+                        checked={allDay}
+                        onChange={event => onChangeAllDay(event.target.checked)}
+                        />
+                </div>
+                <h5>Data source: <a href="https://catalog.data.gov/dataset?tags=%22median+household+income%22" style={{textDecoration: 'none', color: 'blue'}}>ARCGIS DATA</a></h5>
+            </Container>
         );
     }
 }
+
+export default ControlPanel;
