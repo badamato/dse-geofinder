bade4d3655af9722aaff454fcf3014d7e0ec4735
Beth Damato <bethadamato@gmail.com>
2019-06-06 18:27:10 -0400

    adds second call to API - getGeoNameSearch - able to bring back object with onClick

--- ui/src/components/Search/searchFullText.js
+++ ui/src/components/Search/searchFullText.js
@@ -40,11 +40,10 @@ class SearchFullText extends Component {
                 <hr style={{width: '80%', backgroundColor: 'darkgray', height: '1px'}} />
                 <div className={classes.resultsContainer}>
                     <Typography className={classes.results}>
-                        **RESULTS HERE**
-                        **RESULTS HERE**
-                        **RESULTS HERE**
-                        **RESULTS HERE**
-                        **RESULTS HERE**
+                        name <br />
+                        address <br />
+                        city, province, post_code <br />
+                        phone (as link) <br />
                     </Typography>
                 </div>
             </div>
